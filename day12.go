57319cd50145d852026aeccb78a1201bc72f8fa6
Alexey Vasilyev <alexey@alexadotlife.com>
2024-12-12 15:34:33 +0100

    day12 refactor

--- day12.go
+++ day12.go
@@ -6,6 +6,14 @@ import (
 	"os"
 )
 
+type pair struct {
+	x, y int
+}
+type tuple struct {
+	p pair
+	k int
+}
+
 func main() {
 	data := [][]byte{}
 	scanner := bufio.NewScanner(os.Stdin)
@@ -13,15 +21,11 @@ func main() {
 		data = append(data, []byte(scanner.Text()))
 	}
 	m, n := len(data), len(data[0])
-	type pair struct {
-		x, y int
-	}
-	type tuple struct {
-		p pair
-		k int
-	}
-	inBound := func(p pair) bool {
-		return p.x >= 0 && p.x < m && p.y >= 0 && p.y < n
+	getData := func(p pair) (byte, bool){
+		if p.x >= 0 && p.x < m && p.y >= 0 && p.y < n {
+			return data[p.x][p.y], true
+		}
+		return byte(0), false
 	}
 	visited := map[pair]bool{}
 	p1, p2 := 0, 0
@@ -31,68 +35,13 @@ func main() {
 			if _, ok := visited[p]; ok {
 				continue
 			}
-			ch := data[i][j]
-			next := []pair{p}
-			seen := map[pair]bool{}
-			seen[p] = true
-			area := 0
-			borders := map[tuple]bool{}
-			for len(next) > 0 {
-				p := next[0]
-				next = next[1:]
+			seen, borders := explore(p, getData, func(p pair) {
 				visited[p] = true
-				area += 1
-				dx, dy := 1, 0
-				for k := 0; k < 4; k++ {
-					pp := pair{p.x + dx, p.y + dy}
-					if inBound(pp) {
-						ch2 := data[pp.x][pp.y]
-						if ch == ch2 {
-							if _, ok := seen[pp]; !ok {
-								seen[pp] = true
-								next = append(next, pp)
-							}
-						} else {
-							borders[tuple{p, k}] = false
-						}
-					} else {
-						borders[tuple{p, k}] = false
-					}
-					dx, dy = -dy, dx
-				}
-			}
+			})
+			area := len(seen)
 			perimeter := len(borders)
 			p1 += perimeter * area
-			sides := 0
-			for border, counted := range borders {
-				if counted {
-					continue
-				}
-				sides += 1
-				p := border.p
-				dx, dy := 1, 0
-				for k := 0; k < 4; k++ {
-					for times := 1; times < max(n, m); times++ {
-						pp := pair{p.x + dx*times, p.y + dy*times}
-						t := tuple{pp, border.k}
-						if _, ok := borders[t]; ok {
-							borders[t] = true
-						} else {
-							break
-						}
-					}
-					for times := 1; times < max(n, m); times++ {
-						pp := pair{p.x - dx*times, p.y - dy*times}
-						t := tuple{pp, border.k}
-						if _, ok := borders[t]; ok {
-							borders[t] = true
-						} else {
-							break
-						}
-					}
-					dx, dy = -dy, dx
-				}
-			}
+			sides := bordersToSides(borders)
 			p2 += sides * area
 		}
 	}
@@ -100,9 +49,63 @@ func main() {
 	fmt.Println("Part 2:", p2)
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
+func explore(p pair, getData func (p pair) (byte, bool), visit func(p pair)) (map[pair]bool, map[tuple]bool) {
+	ch, _ := getData(p)
+	next := []pair{p}
+	seen := map[pair]bool{}
+	seen[p] = true
+	borders := map[tuple]bool{}
+	for len(next) > 0 {
+		p := next[0]
+		next = next[1:]
+		visit(p)
+		dx, dy := 1, 0
+		for k := 0; k < 4; k++ {
+			pp := pair{p.x + dx, p.y + dy}
+			if ch2, ok := getData(pp); ok && ch == ch2 {
+				if _, ok := seen[pp]; !ok {
+					seen[pp] = true
+					next = append(next, pp)
+				}
+			} else {
+				borders[tuple{p, k}] = false
+			}
+			dx, dy = -dy, dx
+		}
 	}
-	return b
+	return seen, borders
+}
+
+func bordersToSides(borders map[tuple]bool) int {
+	sides := 0
+	for border, counted := range borders {
+		if counted {
+			continue
+		}
+		sides += 1
+		p := border.p
+		dx, dy := 1, 0
+		for k := 0; k < 4; k++ {
+			for times := 1; ; times++ {
+				pp := pair{p.x + dx*times, p.y + dy*times}
+				t := tuple{pp, border.k}
+				if _, ok := borders[t]; ok {
+					borders[t] = true
+				} else {
+					break
+				}
+			}
+			for times := 1; ; times++ {
+				pp := pair{p.x - dx*times, p.y - dy*times}
+				t := tuple{pp, border.k}
+				if _, ok := borders[t]; ok {
+					borders[t] = true
+				} else {
+					break
+				}
+			}
+			dx, dy = -dy, dx
+		}
+	}
+	return sides
 }
